8803ff4283e2024c50e9ff5fbc7a79cc6520cbfa
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-28 22:08:01 -0500

    Update Display 3 Things.py

--- Display 3 Things.py
+++ Display 3 Things.py
@@ -4,4 +4,6 @@
 # Date: 10/23
 # Purpose of the program: Display my name, favorite movie and city
 #=====================================================================
+#if you are not using an input statement, then you should display what to be printed.
+#print("Please tell us your name, your favorite movie, and the city you are living")
 print("Keegan, The Green Mile, Crest Hill")
